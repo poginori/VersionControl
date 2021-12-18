6fca7b44457a4f29d3b0105bd4dbb969b3fde54a
Nóra Pogátsa <nora.varga@stud.uni-corvinus.hu>
2021-12-18 09:17:26 +0100

    kész gépi tanulás

--- Gepitanulas2/Gepitanulas2/Form1.Designer.cs
+++ Gepitanulas2/Gepitanulas2/Form1.Designer.cs
@@ -29,13 +29,46 @@ namespace Gepitanulas2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.generationlabel = new System.Windows.Forms.Label();
+            this.Startbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // generationlabel
+            // 
+            this.generationlabel.AutoSize = true;
+            this.generationlabel.Location = new System.Drawing.Point(306, 370);
+            this.generationlabel.Name = "generationlabel";
+            this.generationlabel.Size = new System.Drawing.Size(92, 20);
+            this.generationlabel.TabIndex = 0;
+            this.generationlabel.Text = "1.generáció";
+            // 
+            // Startbutton
+            // 
+            this.Startbutton.Location = new System.Drawing.Point(310, 402);
+            this.Startbutton.Name = "Startbutton";
+            this.Startbutton.Size = new System.Drawing.Size(75, 36);
+            this.Startbutton.TabIndex = 1;
+            this.Startbutton.Text = "Start";
+            this.Startbutton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Startbutton);
+            this.Controls.Add(this.generationlabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label generationlabel;
+        private System.Windows.Forms.Button Startbutton;
     }
 }
 
--- Gepitanulas2/Gepitanulas2/Form1.cs
+++ Gepitanulas2/Gepitanulas2/Form1.cs
@@ -7,14 +7,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace Gepitanulas2
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
+        Brain winnerBrain;
+
         public Form1()
         {
             InitializeComponent();
+
+            gc.GameOver += Gc_GameOver;
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            generationlabel.Text = string.Format("{0}.generáció", generation);
+            var PlayerList = (from p in gc.GetCurrentPlayers()
+                              orderby p.GetFitness() descending
+                              select p);
+            var topPerformers = PlayerList.Take(populationSize / 2).ToList();
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
+
+            var winners = from p in topPerformers where p.IsWinner select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                Startbutton.Visible = true;
+                return;
+            }
+
+        }
+
+        private void startbutton_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
         }
     }
 }
